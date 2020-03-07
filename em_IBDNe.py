b9980df757d7bb5c0efda0232cbfa25e3758edd9
Yilei Huang <yh362@cornell.edu>
2020-03-06 23:24:38 -0500

    add em to main

--- em_IBDNe.py
+++ em_IBDNe.py
@@ -23,6 +23,8 @@ def main():
     print(f'A total of {np.sum(bin1)+np.sum(bin2)} IBD segments read.')
     print(f'Among them, {np.sum(bin2)} reach chromosome end.')
 
+    N, T1, T2 = em(args.maxGen, bin1, bin2, bin_midPoint1, bin_midPoint2, args.tol, args.maxIter)
+
 if __name__ == '__main__':
     main()
 
