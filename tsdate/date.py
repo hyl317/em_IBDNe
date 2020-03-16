c4bf0f7a85f9819da2050359868a5aefc83d9222
Yilei Huang <yh362@cornell.edu>
2020-03-16 16:06:19 -0400

    wanna try out tsinfer and tsdate suite

---
+++ tsdate/date.py
@@ -0,0 +1,54 @@
+import tsinfer
+import tsdate
+import tskit
+import argparse
+import cyvcf2
+
+def add_diploid_sites(vcf, samples):
+    """
+    Read the sites in the vcf and add them to the samples object, reordering the
+    alleles to put the ancestral allele first, if it is available.
+    """
+    pos = 0
+    for variant in vcf:  # Loop over variants, each assumed at a unique site
+        if pos == variant.POS:
+            raise ValueError("Duplicate positions for variant at position", pos)
+        else:
+            pos = variant.POS
+        if any([not phased for _, _, phased in variant.genotypes]):
+            raise ValueError("Unphased genotypes for variant at position", pos)
+        alleles = [variant.REF] + variant.ALT
+        ancestral = variant.INFO.get('AA', variant.REF)
+        # Ancestral state must be first in the allele list.
+        ordered_alleles = [ancestral] + list(set(alleles) - {ancestral})
+        allele_index = {old_index: ordered_alleles.index(allele)
+            for old_index, allele in enumerate(alleles)}
+        # Map original allele indexes to their indexes in the new alleles list.
+        genotypes = [allele_index[old_index]
+            for row in variant.genotypes for old_index in row[0:2]]
+        samples.add_site(pos, genotypes=genotypes, alleles=alleles)
+
+def chromosome_length(vcf):
+    assert len(vcf.seqlens) == 1
+    return vcf.seqlens[0]
+
+def main():
+    parser = argparse.ArgumentParser(description='Use tsdate to infer node age')
+    parser.add_argument('-v', action="store", dest="vcf", type=str, required=True, 
+                        help='Path to VCF File')
+    args = parser.parse_args()
+
+    vcf = cyvcf2.VCF(args.vcf)
+    with tsinfer.SampleData(path=f"{args.vcf}.samples",
+                        sequence_length=chromosome_length(vcf)) as samples:
+        add_diploid_sites(vcf, samples)
+
+    print("Sample file created for {} samples ".format(samples.num_samples) +
+        "({} individuals) ".format(samples.num_individuals) +
+        "with {} variable sites.".format(samples.num_sites), flush=True)
+
+    # Do the inference
+    ts = tsinfer.infer(samples)
+    print("Inferred tree sequence: {} trees over {} Mb ({} edges)".format(
+        ts.num_trees, ts.sequence_length/1e6, ts.num_edges))
+
