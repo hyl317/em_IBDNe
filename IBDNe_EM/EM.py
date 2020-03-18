8b6b51c43742bf8dcc49ec34dab5ae46c3d48228
Yilei Huang <yh362@cbsubscb04.biohpc.cornell.edu>
2020-03-18 19:36:56 -0400

    a few more bugs spotted; need to add the last column to matrix1 and 2

--- IBDNe_EM/EM.py
+++ IBDNe_EM/EM.py
@@ -26,7 +26,7 @@ def initializeT_Random(numBins, maxGen):
     T = np.random.rand(numBins, maxGen+1)
     return T/T.sum(axis=1)[:, np.newaxis]
 
-def logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, alpha):
+def logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha):
     ##calculate and return the log likelihood of the complete data
     sum_log_prob_not_coalesce = np.cumsum(np.insert(np.log(1-1/(2*N)), 0, 0))
     G = len(N)
@@ -54,12 +54,12 @@ def logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, alpha):
     T2 = np.append(T2, last_col_2[:,np.newaxis], axis=1)
 
     ## add penalty term to the log likelihood
-    #N_shifted = np.roll(N,-1)
-    #N_shifted[-1] = N[-1]
-    #diff = N_shifted - N
-    #penalty = alpha*np.sum(np.dot(diff, diff))/np
+    N_shifted = np.roll(N,-1)
+    N_shifted[-1] = N[-1]
+    diff = N_shifted - N
+    penalty = alpha*np.sum(np.dot(diff, diff))/N_p
 
-    return np.sum(bin1*np.apply_along_axis(logsumexp, 1, T1)) + np.sum(bin2*np.apply_along_axis(logsumexp, 1, T2))
+    return np.sum(bin1*np.apply_along_axis(logsumexp, 1, T1)) + np.sum(bin2*np.apply_along_axis(logsumexp, 1, T2)) + penalty
 
 def eStep(N, bin1, bin2, bin_midPoint1, bin_midPoint2):
     #return updated T1 and T2
@@ -102,11 +102,12 @@ def negCompleteDataLikelihood(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint
     N_shifted = np.roll(N,-1)
     N_shifted[-1] = N[-1]
     diff = N_shifted - N
-    penalty = alpha*np.sum(np.dot(diff, diff))/N_p
+    penalty = (alpha*np.sum(np.dot(diff, diff)))/N_p
 
     sum_log_prob_not_coalesce = np.cumsum(np.insert(np.log(1-1/(2*N)), 0, 0)) #this has length maxGen+1
 
     #for IBD segments in the middle of the chromosome
+    G = len(N)
     log_g_over_50 = np.log(np.arange(1, G+1)/50)
     log_2_times_N_g = np.log(2*N)
     len_times_g_over_50_1 = bin_midPoint1.reshape((len(bin_midPoint1),1))@(np.arange(1, G+1).reshape((1, G)))/50
@@ -114,8 +115,8 @@ def negCompleteDataLikelihood(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint
 
     matrix1 = 2*log_g_over_50 - len_times_g_over_50_1 + sum_log_prob_not_coalesce[:-1] - log_2_times_N_g
     matrix2 = log_g_over_50 - len_times_g_over_50_2 + sum_log_prob_not_coalesce[:-1] - log_2_times_N_g
-    term1 = np.sum(np.exp(T1 + bin1)*matrix1)
-    term2 = np.sum(np.exp(T2 + bin2)*matrix2)
+    term1 = np.sum(np.exp(T1 + bin1[:,np.newaxis])*matrix1)
+    term2 = np.sum(np.exp(T2 + bin2[:,np.newaxis])*matrix2)
     return -term1-term2-penalty
 
 
@@ -175,7 +176,7 @@ def em(maxGen, bin1, bin2, bin_midPoint1, bin_midPoint2, numInds, tol, maxIter):
     print(f"initial N:{N}")
     loglike_prev = logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha)
     T1, T2 = eStep(N, bin1, bin2, bin_midPoint1, bin_midPoint2)
-    N = mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha)
+    N = mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha)
     loglike_curr = logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha)
     num_iter = 1
     plotPosterior(np.exp(T1.T), bin_midPoint1, np.arange(1, maxGen+2), title=f'Posterior Distribution for Iteration {num_iter}')
