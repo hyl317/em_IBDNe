af4741c1df0285d063b4dcdb4e6b32c05962b944
Yilei Huang <yh362@cornell.edu>
2020-03-18 19:27:11 -0400

    previous objective function is wrong

--- IBDNe_EM/EM.py
+++ IBDNe_EM/EM.py
@@ -26,7 +26,7 @@ def initializeT_Random(numBins, maxGen):
     T = np.random.rand(numBins, maxGen+1)
     return T/T.sum(axis=1)[:, np.newaxis]
 
-def negLogLike(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha):
+def logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, alpha):
     ##calculate and return the log likelihood of the complete data
     sum_log_prob_not_coalesce = np.cumsum(np.insert(np.log(1-1/(2*N)), 0, 0))
     G = len(N)
@@ -54,12 +54,12 @@ def negLogLike(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha):
     T2 = np.append(T2, last_col_2[:,np.newaxis], axis=1)
 
     ## add penalty term to the log likelihood
-    N_shifted = np.roll(N,-1)
-    N_shifted[-1] = N[-1]
-    diff = N_shifted - N
-    penalty = alpha*np.sum(np.dot(diff, diff))/np
+    #N_shifted = np.roll(N,-1)
+    #N_shifted[-1] = N[-1]
+    #diff = N_shifted - N
+    #penalty = alpha*np.sum(np.dot(diff, diff))/np
 
-    return -np.sum(bin1*np.apply_along_axis(logsumexp, 1, T1)) - np.sum(bin2*np.apply_along_axis(logsumexp, 1, T2)) - penalty
+    return np.sum(bin1*np.apply_along_axis(logsumexp, 1, T1)) + np.sum(bin2*np.apply_along_axis(logsumexp, 1, T2))
 
 def eStep(N, bin1, bin2, bin_midPoint1, bin_midPoint2):
     #return updated T1 and T2
@@ -97,7 +97,29 @@ def eStep(N, bin1, bin2, bin_midPoint1, bin_midPoint2):
     T2 = T2 - normalizing_constant2
     return T1, T2
 
-def jacobian(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha):
+def negCompleteDataLikelihood(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha):
+    ## add penalty term to the log likelihood
+    N_shifted = np.roll(N,-1)
+    N_shifted[-1] = N[-1]
+    diff = N_shifted - N
+    penalty = alpha*np.sum(np.dot(diff, diff))/N_p
+
+    sum_log_prob_not_coalesce = np.cumsum(np.insert(np.log(1-1/(2*N)), 0, 0)) #this has length maxGen+1
+
+    #for IBD segments in the middle of the chromosome
+    log_g_over_50 = np.log(np.arange(1, G+1)/50)
+    log_2_times_N_g = np.log(2*N)
+    len_times_g_over_50_1 = bin_midPoint1.reshape((len(bin_midPoint1),1))@(np.arange(1, G+1).reshape((1, G)))/50
+    len_times_g_over_50_2 = bin_midPoint2.reshape((len(bin_midPoint2),1))@(np.arange(1, G+1).reshape((1, G)))/50
+
+    matrix1 = 2*log_g_over_50 - len_times_g_over_50_1 + sum_log_prob_not_coalesce[:-1] - log_2_times_N_g
+    matrix2 = log_g_over_50 - len_times_g_over_50_2 + sum_log_prob_not_coalesce[:-1] - log_2_times_N_g
+    term1 = np.sum(np.exp(T1 + bin1)*matrix1)
+    term2 = np.sum(np.exp(T2 + bin2)*matrix2)
+    return -term1-term2-penalty
+
+
+def jacobian(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha):
     maxGen = len(N)
     T1 = np.log(bin1)[:,np.newaxis] + T1
     T2 = np.log(bin2)[:,np.newaxis] + T2
@@ -116,13 +138,13 @@ def jacobian(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha):
     penalty_term = 4*N - 2*(N_left + N_right)
     penalty_term[0] = 2*(N[0] - N[1])
     penalty_term[-1] = 2*(N[-1] - N[-2])
-    print(f'penalty component in gradient: {alpha*penalty_term/np}')
-    return -np.exp(likelihood_term) - alpha*penalty_term/np
+    #print(f'penalty component in gradient: {alpha*penalty_term/np}')
+    return -np.exp(likelihood_term) - alpha*penalty_term/N_p
 
 
-def mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha):
+def mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha):
     bnds = [(0, np.inf) for n in N]
-    result = minimize(negLogLike, N, args=(T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha), 
+    result = minimize(negCompleteDataLikelihood, N, args=(T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha), 
                       method='L-BFGS-B', tol=1e-6, bounds=bnds, jac=jacobian)
     ##return the updated N estimate
     #maxGen = len(N)
@@ -148,21 +170,21 @@ def mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha):
 
 def em(maxGen, bin1, bin2, bin_midPoint1, bin_midPoint2, numInds, tol, maxIter):
     alpha = 0.05
-    np = 2*numInds*(2*numInds-2)/2
+    N_p = 2*numInds*(2*numInds-2)/2
     N, T1, T2 = initializeN_autoreg(maxGen), initializeT_Random(bin1.shape[0], maxGen), initializeT_Random(bin2.shape[0], maxGen)
     print(f"initial N:{N}")
-    loglike_prev = -negLogLike(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha)
+    loglike_prev = logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha)
     T1, T2 = eStep(N, bin1, bin2, bin_midPoint1, bin_midPoint2)
     N = mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha)
-    loglike_curr = -negLogLike(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha)
+    loglike_curr = logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha)
     num_iter = 1
     plotPosterior(np.exp(T1.T), bin_midPoint1, np.arange(1, maxGen+2), title=f'Posterior Distribution for Iteration {num_iter}')
     while (loglike_curr - loglike_prev >= tol and num_iter < maxIter):
         print(f'iteration{num_iter} done. Likelihood improved by {loglike_curr-loglike_prev}')
         loglike_prev = loglike_curr
         T1, T2 = eStep(N, bin1, bin2, bin_midPoint1, bin_midPoint2)
-        N = mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha)
-        loglike_curr = -negLogLike(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, np, alpha)
+        N = mStep(N, T1, T2, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha)
+        loglike_curr = logLike(N, bin1, bin2, bin_midPoint1, bin_midPoint2, N_p, alpha)
         num_iter += 1
     
     print(f'iteration{num_iter} done. Likelihood improved by {loglike_curr-loglike_prev}')
