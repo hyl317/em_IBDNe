ceace2d2b76f51045c58a46b634f2be3aa707abb
Yilei Huang <yh362@cornell.edu>
2020-04-06 12:12:15 -0400

    add something

--- IBDNe_EM/effective_sample_size.py
+++ IBDNe_EM/effective_sample_size.py
@@ -3,6 +3,20 @@ import gzip
 import bisect
 import argparse
 
+L = 3500 #set total genome length to be 3500cM
+
+def expectation_num_segment(N, u):
+    G = len(N)
+    gen = np.arange(1, G+1)
+    sum_log_prob_not_coalesce = np.cumsum(np.insert(np.log(1-1/(2*N)), 0, 0))
+    log_term1 = logsumexp(np.log(gen/50)-u*gen/50 - np.log(2*N) + sum_log_prob_not_coalesce[:-1])
+
+    N_G = N[-1]
+    alpha = np.log(1-1/(2*N_G))-u/50
+
+    log_term2 = -np.log(100*N_G) + sum_log_prob_not_coalesce[-1] + (G+1)*(np.log(2*N_G)-np.log(2*N_G-1)) + alpha*(G+1) + np.log(1+G*(1-np.exp(alpha))) - 2*np.log(1-np.exp(alpha))
+    return L*(np.exp(log_term1)+np.exp(log_term2))
+
 
 def process_ibd_hbd(ibd, hbd, bins, num_haps):
     #read ibd.gz and hbd.gz file
@@ -77,12 +91,36 @@ def main():
     parser.add_argument('--ibd', action="store", dest="ibd", type=str, required=True)
     parser.add_argument('--hbd', action="store", dest="hbd", type=str, required=True)
     parser.add_argument('-n', action="store", dest='n', type=int, required=True)
+    parser.add_argument('--bins', action="store", dest='bins', type=str, required=False)
+    parser.add_argument('-N', action="store", dest="N", type=str, required=False, help="path to file containing reference population size")
     args = parser.parse_args()
 
-    bin = [2, 3, 4, 5, 6, 7, 8, 9, 10]
-    effective_sample_size, mean_IBD_count = process_ibd_hbd(args.ibd, args.hbd, bin, args.n)
-    print(f'effective_sample_size={effective_sample_size}')
-    print(f'mean={np.flip(np.cumsum(np.flip(mean_IBD_count)))}')
+    bins = []
+    if args.bins == None:
+        bins = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+    else:
+        tmp = args.bins.strip().split(',')
+        bins = [float(p) for p in tmp]
+
+    #if reference Ne trajectory is provided, calculate the expected number of IBD segments
+    if args.N != None:
+        with open(args.N) as file_N:
+            line = file_N.readline()
+            while line:
+                gen, Ne = line.strip().split('\t')
+                Ne = float(Ne)
+                N.append(Ne)
+                line = file_N.readline()
+        N = np.array(N)
+
+        expected_IBD_count = [expectation_num_segment(N, b) for b in bins]
+
+    effective_sample_size, mean_IBD_count = process_ibd_hbd(args.ibd, args.hbd, bins, args.n)
+    print(f'mean: {np.flip(np.cumsum(np.flip(mean_IBD_count)))}')
+    if args.N != None:
+        print(f'expected: {expected_IBD_count}')
+    print(f'effective_sample_size: {effective_sample_size}')
+    
 
 if __name__ == '__main__':
     main()
