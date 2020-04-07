97b9d7992dc51206b9876696c21d1105fa5a10cb
Yilei Huang <yh362@cornell.edu>
2020-04-06 20:05:23 -0400

    add another feature
    redistribute censored IBD

--- IBDNe_EM/effective_sample_size.py
+++ IBDNe_EM/effective_sample_size.py
@@ -4,9 +4,7 @@ import bisect
 import argparse
 from scipy.special import logsumexp
 
-L = 3500 #set total genome length to be 3500cM
-
-def expectation_num_segment(N, u):
+def expectation_num_segment(N, u, total_genome_length):
     G = len(N)
     gen = np.arange(1, G+1)
     sum_log_prob_not_coalesce = np.cumsum(np.insert(np.log(1-1/(2*N)), 0, 0))
@@ -16,17 +14,39 @@ def expectation_num_segment(N, u):
     alpha = np.log(1-1/(2*N_G))-u/50
 
     log_term2 = -np.log(100*N_G) + sum_log_prob_not_coalesce[-1] + (G+1)*(np.log(2*N_G)-np.log(2*N_G-1)) + alpha*(G+1) + np.log(1+G*(1-np.exp(alpha))) - 2*np.log(1-np.exp(alpha))
-    return L*(np.exp(log_term1)+np.exp(log_term2))
+    return total_genome_length*(np.exp(log_term1)+np.exp(log_term2))
 
 
-def process_ibd_hbd(ibd, hbd, bins, num_haps):
+def process_ibd_hbd(ibd, hbd, endMarkers, bins, num_haps):
     #read ibd.gz and hbd.gz file
     #return effective sample size for each bin and mean number of IBD segments in each bin
 
     hapPair2Index = {}
     n_pairs = int(num_haps*(num_haps-1)/2)
     IBD_matrix = np.zeros((n_pairs, len(bins)))
+    IBD_count_censored = np.zeros(len(bins))
     count = 0
+
+    #read in end markers
+    endMarker_bp = {}
+    endMarker_cM = {}
+
+    with open(end_marker) as end:
+        line = end.readline()
+        while line:
+            chr, rate, cM, phy = line.strip().split('\t')
+            chr, cM, phy = int(chr), float(cM), int(phy)
+            if not chr in endMarker_bp:
+                endMarker_bp[chr] = [phy]
+                endMarker_cM[chr] = [cM]
+            else:
+                endMarker_bp[chr].append(phy)
+                endMarker_cM[chr].append(cM)
+            line = end.readline()
+
+    total_genome_length = 0
+    for chr, start_end in endMarker_cM.items():
+        total_genome_length += abs(start_end[1]-start_end[0])
 
     with gzip.open(ibd, 'rt') as ibd:
         line = ibd.readline()
@@ -40,6 +60,9 @@ def process_ibd_hbd(ibd, hbd, bins, num_haps):
             
             tmp = bisect.bisect_left(bins, len_cM)
             pos = tmp if (tmp < len(bins) and bins[tmp] == len_cM) else tmp-1
+
+            if start_bp in endMarker_bp[chr] or end_bp in endMarker_bp[chr]:
+                IBD_count_censored[pos] += 1
 
             if hapPair not in hapPair2Index:
                 hapPair2Index[hapPair] = count
@@ -63,6 +86,9 @@ def process_ibd_hbd(ibd, hbd, bins, num_haps):
             tmp = bisect.bisect_left(bins, len_cM)
             pos = tmp if (tmp < len(bins) and bins[tmp] == len_cM) else tmp-1
 
+            if start_bp in endMarker_bp[chr] or end_bp in endMarker_bp[chr]:
+                IBD_count_censored[pos] += 1
+
             if hapPair not in hapPair2Index:
                 hapPair2Index[hapPair] = count
                 IBD_matrix[count, pos] += 1
@@ -84,14 +110,25 @@ def process_ibd_hbd(ibd, hbd, bins, num_haps):
         bootstrap_variance[j] = np.var(bootstrapped_mean)
 
     #now IBD_matrix is filled
-    mean = np.apply_along_axis(np.mean, 0, IBD_matrix)
-    return mean/bootstrap_variance, mean
+    count_total_IBD = np.apply_along_axis(np.sum, 0, IBD_matrix)
+    tmp = np.copy(count_total_IBD)
+    count_uncensored_IBD = count_total_IBD - IBD_count_censored
+
+    #redistribute censored IBD into bins
+    frac = count_total_IBD/np.sum(count_total_IBD)
+    for b, count in enumerate(IBD_count_censored):
+        tmp += (count*frac[b:])/np.sum(frac[b:])
+
+    mean = tmp/n_pairs
+
+    return mean/bootstrap_variance, mean, total_genome_length
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--ibd', action="store", dest="ibd", type=str, required=True)
     parser.add_argument('--hbd', action="store", dest="hbd", type=str, required=True)
-    parser.add_argument('-n', action="store", dest='n', type=int, required=True)
+    parser.add_argument('-e', action='store', dest='end', type='str', required=True, help="path to files of end markers")
+    parser.add_argument('-n', action="store", dest='n', type=int, required=True, help="number of haplotypes")
     parser.add_argument('--bins', action="store", dest='bins', type=str, required=False)
     parser.add_argument('-N', action="store", dest="N", type=str, required=False, help="path to file containing reference population size")
     args = parser.parse_args()
@@ -103,6 +140,9 @@ def main():
         tmp = args.bins.strip().split(',')
         bins = [float(p) for p in tmp]
 
+    effective_sample_size, mean_IBD_count, total_genome_length = process_ibd_hbd(args.ibd, args.hbd, args.end, bins, args.n)
+
+    #calculate expected number of IBD segments
     #if reference Ne trajectory is provided, calculate the expected number of IBD segments
     if args.N != None:
         N = []
@@ -115,13 +155,12 @@ def main():
                 line = file_N.readline()
         N = np.array(N)
 
-        expected_IBD_count = [expectation_num_segment(N, b) for b in bins]
+        expected_IBD_count = [expectation_num_segment(N, b, total_genome_length) for b in bins]
 
-    effective_sample_size, mean_IBD_count = process_ibd_hbd(args.ibd, args.hbd, bins, args.n)
+
     print(f'mean: {np.flip(np.cumsum(np.flip(mean_IBD_count)))}')
     if args.N != None:
         print(f'expected: {expected_IBD_count}')
-    print(f'effective_sample_size: {effective_sample_size}')
     
 
 if __name__ == '__main__':
